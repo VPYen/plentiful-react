5185c5430d611c655636c0b9d187e3c58444aa35
VYen <yensailin@gmail.com>
2019-02-08 14:17:10 -0800

    instantiated Spring Boot back-end api

---
+++ api/PlentifulApi/src/main/java/javaSpring/Project/PlentifulApi/PlentifulApiApplication.java
@@ -0,0 +1,14 @@
+package javaSpring.Project.PlentifulApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlentifulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlentifulApiApplication.class, args);
+	}
+
+}
+
